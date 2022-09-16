6d5cef0d24b8c7dd39889db730522bf7430e06d7
zx <2071916845@qq.com>
2022-09-16 20:47:17 +0800

    导航栏

--- themes/butterfly/source/global.js
+++ themes/butterfly/source/global.js
@@ -134,3 +134,37 @@ function percent() {
         btn.childNodes[0].style.display = 'block'
     }
 }
+
+//js有一个小问题：就是只要鼠标滚动不论哪里都会响应，即便你滚动的是子元素
+document.getElementById("name-container").setAttribute("style","display:none");
+
+var scrollFunc = function (e) {
+    var e = e || window.event;
+    if (e.wheelDelta) {
+      if (e.wheelDelta > 0) {
+        document.getElementsByClassName("menus_items")[1].setAttribute("style","");
+        document.getElementById("name-container").setAttribute("style","display:none");
+      }
+      if (e.wheelDelta < 0) { 
+        document.getElementById("name-container").setAttribute("style","");
+        document.getElementsByClassName("menus_items")[1].setAttribute("style","display:none!important");
+      }
+    } else if (e.detail) {
+      if (e.detail < 0) { 
+        document.getElementsByClassName("menus_items")[1].setAttribute("style","");
+        document.getElementById("name-container").setAttribute("style","display:none");
+      }
+      if (e.detail > 0) { 
+        document.getElementById("name-container").setAttribute("style","");
+        document.getElementsByClassName("menus_items")[1].setAttribute("style","display:none!important");
+      }
+    }
+  }
+  window.addEventListener("DOMMouseScroll", scrollFunc)
+  window.addEventListener("wheel", scrollFunc)  
+
+  document.getElementById("page-name").innerText=document.title
+// document.getElementById("page-name").innerText=document.title.split(" | LYXの小破站")[0]; 
+/*这里是去掉你的网站全局名称的设置，如果你不需要去掉，你可以写成：
+document.getElementById("page-name").innerText=document.title
+或者把你的网站的分隔符和全局网站名称加上去*/
